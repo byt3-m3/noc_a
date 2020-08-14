d2f094af11ef9e8d0c2985b1cd3d2e814dc9652c
byt3-m3 <cbaxtertech@gmail.com>
2020-08-14 22:13:48 +0300

    added YANG support

--- services/yang_test.py
+++ services/yang_test.py
@@ -3,11 +3,11 @@ from ncclient import manager
 
 def main():
     with manager.connect_ssh("10.99.7.0", username="cisco", password='cisco') as m:
-        for i in m.server_capabilities:
-            print(i)
+        # for i in m.server_capabilities:
+        #     print(i)
 
         sla_schema = m.get_schema(
-            'urn:ietf:params:xml:ns:yang:smiv2:CISCO-IPSLA-JITTER-MIB?module=CISCO-IPSLA-JITTER-MIB&revision=2007-07-24')
+            'CISCO-IPSLA-JITTER-MIB')
         print(sla_schema)
 
 
