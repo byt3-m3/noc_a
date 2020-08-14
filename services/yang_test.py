eed1c28392a64c6e6737d17e55fb5fc21068636a
byt3-m3 <cbaxtertech@gmail.com>
2020-08-14 22:41:00 +0300

    added YANG support

--- services/yang_test.py
+++ services/yang_test.py
@@ -3,7 +3,9 @@ from ncclient import manager
 
 def main():
     with manager.connect_ssh("10.99.7.0", username="cisco", password='cisco') as m:
-        sla_schema = m.get_schema('oc-acl-cisco')
+        # for i in m.server_capabilities:
+        #     print(i)
+        sla_schema = m.get_schema('cisco-xe-openconfig-if-ethernet-deviation')
         print(sla_schema)
 
 
