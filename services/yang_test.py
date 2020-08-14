c0e1c98a4488fe26f448e2e0c631682b834ce77e
byt3-m3 <cbaxtertech@gmail.com>
2020-08-15 00:27:43 +0300

    added YANG support

--- services/yang_test.py
+++ services/yang_test.py
@@ -5,8 +5,8 @@ def main():
     with manager.connect_ssh("10.99.7.0", username="cisco", password='cisco') as m:
         # for i in m.server_capabilities:
         #     print(i)
-        # sla_schema = m.get_schema('ietf-interfaces')
-        # print(sla_schema)
+        sla_schema = m.get_schema('ENTITY-MIB')
+        print(sla_schema)
         print(m.get("""
         <filter>
             <top xmlns="ietf-interfaces">
